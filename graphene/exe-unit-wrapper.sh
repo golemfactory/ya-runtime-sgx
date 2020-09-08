3595dd3c1f14f5f2ff6a69773b8cd0af6266bb26
borysp <borysp@invisiblethingslab.com>
2020-09-08 20:26:59 +0200

    Package ready version

--- graphene/exe-unit-wrapper.sh
+++ graphene/exe-unit-wrapper.sh
@@ -4,16 +4,15 @@ set -e
 
 default_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 
+NSJAIL_PATH=${NSJAIL_PATH:-"/opt/yagna-wasi-sgx/nsjail"}
+GRAPHENE_DIR=${GRAPHENE_DIR:-"/opt/yagna-wasi-sgx/graphene/"}
+YAGNA_DIR=${YAGNA_DIR:-"/opt/yagna-wasi-sgx/yagna"}
+
 run_in_nsjail() {
-    "$NSJAIL_PATH" -Mo -R /bin -R /lib -R /lib64 -R /usr -R /etc -R /run -R "$ENCLAVE_KEY_PATH" -R "$GRAPHENE_DIR:/graphene" -R /dev/urandom -B /var/run/aesmd/aesm.socket -B "`pwd`:/work" --cwd /work -E "PATH=$default_path" -- $@
+    "$NSJAIL_PATH" -Mo -R /bin -R /lib -R /lib64 -R /usr -R /etc -R /run -R "$ENCLAVE_KEY_PATH" -R "$GRAPHENE_DIR:/graphene" -R /dev/urandom -B /var/run/aesmd/aesm.socket -B "$YAGNA_DIR:/work" --cwd /work -E "PATH=$default_path" -- $@
 }
 
 run() {
-    if [ ! -v YAGNA_DIR ]; then
-        echo "you need to specify path to the yagna directory!"
-        exit 1
-    fi
-
     local agreement_path=""
     local work_dir=""
     while getopts "a:b:c:w:" name; do
@@ -62,35 +61,25 @@ run() {
 }
 
 sign() {
-    if [ ! -v ENCLAVE_KEY_PATH ]; then
-        echo "You need to specify path to the enclave key!"
-        exit 1
-    fi
-
-    echo "SIGNING ya-runtime-wasi"
     run_in_nsjail /graphene/Pal/src/host/Linux-SGX/signer/pal-sgx-sign --output ya-runtime-wasi.manifest.sgx --libpal /graphene/Runtime/libpal-Linux-SGX.so --key $ENCLAVE_KEY_PATH --manifest ya-runtime-wasi.manifest --exec ya-runtime-wasi
-    run_in_nsjail /graphene/Pal/src/host/Linux-SGX/signer/pal-sgx-get-token --sig ya-runtime-wasi.sig --output ya-runtime-wasi.token
 
-    echo "SIGNING exe-unit"
     run_in_nsjail /graphene/Pal/src/host/Linux-SGX/signer/pal-sgx-sign --output exe-unit.manifest.sgx --libpal /graphene/Runtime/libpal-Linux-SGX.so --key $ENCLAVE_KEY_PATH --manifest exe-unit.manifest --exec exe-unit
+}
+
+token() {
+    run_in_nsjail /graphene/Pal/src/host/Linux-SGX/signer/pal-sgx-get-token --sig ya-runtime-wasi.sig --output ya-runtime-wasi.token
     run_in_nsjail /graphene/Pal/src/host/Linux-SGX/signer/pal-sgx-get-token --sig exe-unit.sig --output exe-unit.token
 }
 
 main() {
-    if [ ! -v NSJAIL_PATH ]; then
-        echo "You need to specify path to the nsjail binary!"
-        exit 1
-    fi
-    if [ ! -v GRAPHENE_DIR ]; then
-        echo "You need to specify path to the Graphene directory!"
-        exit 1
-    fi
-
     case $1
     in
         sign)
             shift
             sign $@
+            ;;
+        token)
+            token
             ;;
         *)
             run $@
